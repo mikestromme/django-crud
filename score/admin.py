fbaaa43cdc0c51d26d1069603994b6d013e19e64
Mike Stromme <mikestromme@gmail.com>
2024-02-06 18:26:39 -0600

    x

--- score/admin.py
+++ score/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from score.models import Score
 
-
+# show all columns in the admin console instead of just 
+# the name which line 10 does if this isn't included
 class ScoreAdmin(admin.ModelAdmin):
     list_display = ['name', 'value']
 
