6dbba098241a5826d57b360b8f792c650ed64769
Mike Stromme <mikestromme@gmail.com>
2024-02-08 05:53:33 -0600

    x

--- crud/settings.py
+++ crud/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_material.apps.AdminMaterialDashboardConfig',
+    #'admin_material.apps.AdminMaterialDashboardConfig',
     #'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
